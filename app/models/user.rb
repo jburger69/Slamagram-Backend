44cf8f820a6d118b3137fcccc22d3621d46b2e74
Joshua Burger <joshberger6969@outlook.com>
2021-06-22 21:04:40 -0400

    Create relationships within user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+
+    has_many :posts
+    has_many :comments, foreign_key: :author_id, class_name: "Comment"
+    has_many :commented_posts, through: :comments, source: :author
+
 end
