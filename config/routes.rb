514c814d60c393d165950d2d0a8913406a69878a
Joshua Burger <joshberger6969@outlook.com>
2021-06-22 21:05:33 -0400

    Create new Api/V1 folders to hold controllers with namespace, Comments are nested within posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :posts
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :comments
+  # resources :posts
+  # resources :users
+
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :posts do
+        resources :comments
+      end
+    end
+  end
+  
 end
