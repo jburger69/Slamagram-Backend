9d30d159b14ddc683f1f12be6736d783f9ddb3a5
Joshua Burger <joshberger6969@outlook.com>
2021-07-01 14:34:07 -0400

    New data to seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,73 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+Post.delete_all
+Comment.delete_all
+User.delete_all
+
+
+
+
+User.create!([{
+    id: 1,
+    username: "joshua69",
+    password: "password",
+    description: "Love playing sports"
+},
+{
+    id: 2,
+    username: "joshua72",
+    password: "password",
+    description: "Coding is life!"
+}
+])
+
+
+Post.create!([{
+    content: "This is what I found today!",
+    image_url: Faker::LoremPixel.image,
+    user_id: 1
+},
+{
+    content: "I love this picture!",
+    image_url: Faker::LoremPixel.image,
+    user_id: 1
+},
+{
+    content: "This is what I found today!",
+    image_url: Faker::LoremPixel.image,
+    user_id: 1
+},
+{
+    content: "This is what I found today!",
+    image_url: Faker::LoremPixel.image,
+    user_id: 1
+},
+{
+    content: "This is what I found today!",
+    image_url: Faker::LoremPixel.image,
+    user_id: 1
+},
+{
+    content: "This is what I found today!",
+    image_url: Faker::LoremPixel.image,
+    user_id: 1
+}
+])
+
+
+
+Comment.create!([{
+    content: "Wow thats amazing!",
+    post_id: 1,
+    author_id: 1
+},
+{
+    content: "I really like this picture!",
+    post_id: 2,
+    author_id: 1 
+}
+])
